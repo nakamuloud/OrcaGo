f4f5a00bc9f83ed6dd2a0b4f1fc45350ae5a8bd5
nakamuloud <im.masayanakamura@gmail.com>
2020-05-16 17:28:29 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import * as VueGoogleMaps from 'vue2-google-maps'
-import config from '../config.json'
 Vue.use(VueGoogleMaps, {
   load: {
     key: "AIzaSyCCnDP91XS-TV1l8zRq3aP7W1kJj6v3aGg",
